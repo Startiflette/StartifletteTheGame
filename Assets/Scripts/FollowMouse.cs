6187cbd6445bffe463925f565a0066c1f8d4dbf8
Auff <adrien.auffredou@gmail.com>
2018-01-08 09:48:51 +0100

    Add axel movement script

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour {
+
+	public float smoothSpeed = 4f;
+
+    public Vector3 plan;
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 desiredPos = Camera.main.GetComponent<Camera> ().ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 6f));
+		Vector3 smoothedPos = Vector3.Lerp (transform.position, desiredPos, smoothSpeed * Time.deltaTime);
+		transform.position = smoothedPos;
+
+        transform.LookAt(desiredPos + plan); 
+	}
+}
