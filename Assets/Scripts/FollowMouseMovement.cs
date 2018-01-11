97f6766efedc8bbc04338df540897f59b848ef1c
Ethoytaryn <briand.guillaume@outlook.fr>
2018-01-11 17:27:03 +0100

    update FollowMouseMovement
    script to add into player with canvas

--- Assets/Scripts/FollowMouseMovement.cs
+++ Assets/Scripts/FollowMouseMovement.cs
@@ -5,27 +5,32 @@ using UnityEngine;
 public class FollowMouseMovement : MonoBehaviour {
 
 	public float speed = 30.0F;
+	public Canvas canvas;
+	Transform[] imageTransform;
+
 	float yMinLimit = -Screen.height / 2;
 	float yMaxLimit = Screen.height / 2;
 	float xMinLimit = - Screen.width / 2;
 	float xMaxLimit = Screen.width / 2;
-
+	float distance;
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("y max =" + yMaxLimit);
-		Debug.Log ("x max =" + xMaxLimit);
+		
+		distance = Screen.height * 0.5f / Mathf.Tan (60 * 0.5f * Mathf.Deg2Rad);
 		float initialX = 0;
 		float initialY = 0;
-		transform.position = new Vector3(initialX, initialY, 700);
+		imageTransform = canvas.GetComponentsInChildren<RectTransform> ();
+
+		imageTransform[1].position = new Vector3(initialX, initialY, distance);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		float origineX = transform.position.x;
-		float origineY = transform.position.y;
-		float translationX = Input.GetAxis ("Mouse X");
-		float translationY = Input.GetAxis("Mouse Y");
+		float origineX = imageTransform[1].position.x;
+		float origineY = imageTransform[1].position.y;
+		float translationX = Input.GetAxis ("Vertical");
+		float translationY = Input.GetAxis("Horizontal");
 
 		float translationFinalX = origineX + translationX * Time.deltaTime*speed;
 		translationFinalX = checkXlimit (translationFinalX);
@@ -33,7 +38,7 @@ public class FollowMouseMovement : MonoBehaviour {
 		float translationFinalY = origineY + translationY * Time.deltaTime*speed;
 		translationFinalY = checkYlimit (translationFinalY);
 
-		transform.position = new Vector3(translationFinalX, translationFinalY, 700);
+		imageTransform[1].position = new Vector3(translationFinalX, translationFinalY, distance);
 	}
 
 	private float checkXlimit(float position){
