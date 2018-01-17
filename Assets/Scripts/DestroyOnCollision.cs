9b7b9810ca3c47e17c4058905e5f2cc8e185bf0f
Unknown <robin.cornec@epsi.fr>
2018-01-17 17:00:35 +0100

    Shoot Rocket + meta Spawn (forgot)

---
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour {
+    void OnCollisionEnter(Collision col)
+    {
+        Destroy(gameObject);
+    }
+}
