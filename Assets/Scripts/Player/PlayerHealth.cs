4411e152c0d1e4d77cf8fdbf121fc528e3610be8
Ethoytaryn <briand.guillaume@outlook.fr>
2018-01-18 16:05:23 +0100

    feature life player
    Add :
    HUB prefab
    HUB Health prefab
    Script heath player
    HealthBar prefab

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int maxLife = 100;                                   // The amount of health the player starts the game with.
+	public int currentHealth;                                   // The current health the player has.
+	public Slider healthSlider;                                 // Reference to the UI's health bar.
+
+	bool isAlive;                                                // Whether the player is dead.
+
+	void Start ()
+	{
+		// Set the initial health of the player.
+		currentHealth = maxLife;
+		isAlive = true;
+		healthSlider.minValue = 0;
+		healthSlider.maxValue = maxLife;
+	}
+
+
+	void Update ()
+	{
+	}
+		
+	public void TakeDamage (int amount)
+	{
+		// Reduce the current health by the damage amount.
+		currentHealth -= amount;
+
+		// If the player has lost all it's health and the death flag hasn't been set yet...
+		if (currentHealth <= 0) {
+			Death ();
+			healthSlider.value = 0;
+		} else {
+			healthSlider.value = currentHealth;
+		}
+	}
+
+	public void Heal (int amount)
+	{
+		currentHealth += amount;
+		if(currentHealth > maxLife)
+		{
+			currentHealth = maxLife;
+		}
+
+		healthSlider.value = currentHealth;
+	}
+
+	public bool IsAlive {
+		get {
+			return isAlive;
+		}
+	}
+		
+	void Death ()
+	{
+		// Set the death flag so this function won't be called again.
+		isAlive = false;
+
+	}       
+}
