19ae05fe73dc226ef6d97d4f58659862d7a61716
Herr-Nest <fonteneau.adrien@gmail.com>
2018-01-19 16:44:09 +0100

    replay

---
+++ Assets/Scripts/EndOfMap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EndOfMap : MonoBehaviour {
+	public Button button;
+	public GameObject gameManager;
+	private Scene scene;
+	// Use this for initialization
+	void Start () {
+		button.gameObject.SetActive (false);
+		scene = gameManager.GetComponentInChildren<Scene> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.CompareTag ("Player")) {
+			
+			button.gameObject.SetActive (true);	
+			Cursor.visible = true;
+
+		}
+	}
+}
