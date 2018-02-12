97cdc47a23e43f392db04934336cc99ef0d5f88a
Unknown <robin.cornec@epsi.fr>
2018-02-12 19:56:34 +0200

    Auto stash before merge of "dev" and "origin/dev"
    c axel ka fé parceke g ri1 foutu

---
+++ Assets/Scripts/Flash.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flash : MonoBehaviour {
+
+    private bool flash = true;
+    private Image image;
+
+    public void Start() {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (flash) {
+            image.color = new Color(255f, 255f, 255f, image.color.a - Time.deltaTime * 4);
+            Debug.Log(image.color);
+            if (image.color.a <= 0) {
+                image.color = new Color(255f, 255f, 255f, 0);
+                flash = false;
+            }
+        } else {
+            Debug.Log(image.color);
+            image.color = new Color(255f, 255f, 255f, image.color.a + Time.deltaTime * 4);
+            if (image.color.a >= 1) {
+                image.color = new Color(255f, 255f, 255f, 0.25f);
+                flash = true;
+            }
+        }
+    }
+}
